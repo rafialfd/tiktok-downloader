5d2f814aff3d0c46fad5698eea83e67714c45dab
rafialfd <134205268+rafialfd@users.noreply.github.com>
2025-10-06 11:59:10 +0700

    updating server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 require("dotenv").config();
 
 app.use(express.json());
 app.use(express.static("public"));
 
 const API = process.env.API;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 app.post("/get-video", (req, res) => {
   const videoURL = encodeURIComponent(req.body.url);
