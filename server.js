ecf4a2f2d856a440308172799c75c90cd6ef1670
rafialfd <134205268+rafialfd@users.noreply.github.com>
2025-10-06 11:39:50 +0700

    adding file

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+app.use(express.static("public"));
+
+const API = process.env.API;
+
+app.post("/get-video", (req, res) => {
+  const videoURL = encodeURIComponent(req.body.url);
+  const host = "https://tiktok-scraper7.p.rapidapi.com/?url=";
+  const url = host + videoURL;
+
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "79fea81cd0msh36d226ee2009aa6p1e3ff7jsn439118faeb23",
+      "x-rapidapi-host": "tiktok-scraper7.p.rapidapi.com",
+    },
+  })
+    .then((result) => result.json())
+    .then((data) => res.json(data));
+});
+
+app.listen(3000);
